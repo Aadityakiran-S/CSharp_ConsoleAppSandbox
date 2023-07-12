68e476ad5fda39338ee69c34480f27ff99c1f108
Aadityakiran S <aadityakirans@gmail.com>
2023-07-12 11:35:49 +0530

    Project Setup and test run

---
+++ LLDTestGround_C#/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+
+        Program myProgram = new Program();
+        myProgram.PrintAllElementsInArray(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 });
+    }
+
+    public void PrintAllElementsInArray(int[] input)
+    {
+        foreach(var element in input)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
